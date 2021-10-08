c1438a3336e4351a9f898326dd9409735827e133
ixtitricubics <ixti@tricubics.com>
2021-10-08 17:47:59 +0900

    new changes

--- detector/YOLOv4/__init__.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.append("detector/YOLOv4")
-from .detector import YOLOv4
-__all__ = ['YOLOv4']
-
-
-
--- detector/YOLOv4/run.sh
+++
@@ -1,1 +0,0 @@
-python darknet_video.py --input /home/ixti/Documents/projects/video/bottle_can_cup_milk_1.mp4 --weights yolov4_final.weights --config_file yolov4.cfg --data_file drink_ix2.data --thresh 0.5
